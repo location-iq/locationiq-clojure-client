a32ceaa9f3133caf6d0e042b6465f2243c91417c
arvind@unwiredlabs.com <arvind@unwiredlabs.com>
2018-12-03 21:21:43 +0530

    Added support for additional place types in address details section.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject location-iq "1.0.0"
+(defproject location-iq "1.0.1"
   :description "LocationIQ provides flexible enterprise-grade location based solutions. We work with developers, startups and enterprises worldwide serving billions of requests everyday. This page provides an overview of the technical aspects of our API and will help you get started."
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "3.6.0"]
